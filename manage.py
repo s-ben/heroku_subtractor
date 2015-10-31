a4bec5cdf694107e7edeff2c38becc20c39f01d3
s-ben <seth.benton@gmail.com>
2015-10-31 12:04:20 -0700

    initial commit of new git repo for 'application root' (subtractor). I.e. this is the existing app, minus the 'project root', which is keeping me from deploying to heroku

---
+++ manage.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import os
+import sys
+
+
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "subtractor.settings")
+
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
+
+    # SECRET_KEY = 'v5i%t-2)!&uateyb7jzml%k8@fgo4fxgb#2@1!7gmce40%yc@&'
---
+++ subtractor/wsgi.py
@@ -0,0 +1,23 @@
+"""
+WSGI config for subtractor project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+from dj_static import Cling
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "subtractor.settings")
+
+application = Cling(get_wsgi_application())
+
+
+
+# original code, deferring to heroku code above for now...
+# application = get_wsgi_application()
